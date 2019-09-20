66c61254241913fc14c102540479bc793886b272
elanderos <elanderos318@gmail.com>
2019-09-20 16:33:11 -0500

    Updates

---
+++ precache-manifest.a2848b13f9eb2518d540fa065b305a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c40e41bdb3af84566e56af1ab817841",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "cc646ff293324a57a04d",
+    "url": "/portfolio-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6955cbed956b836ce9ff",
+    "url": "/portfolio-website/static/js/2.0e9117d4.chunk.js"
+  },
+  {
+    "revision": "cc646ff293324a57a04d",
+    "url": "/portfolio-website/static/js/main.eecfef99.chunk.js"
+  },
+  {
+    "revision": "00f2a64c24c1b1ef60ef",
+    "url": "/portfolio-website/static/js/runtime~main.d553bd34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
